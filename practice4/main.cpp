935b2e9f5a083af25009779b93fa2a36e011dc7e
Xore <bogomazov2007@yandex.ru>
2025-10-19 21:43:21 +0300

    fix

--- practice4/main.cpp
+++ practice4/main.cpp
@@ -10,7 +10,8 @@ int main() {
 	cin >> x;
 	cin >> y;
 
-	float S = tan(x/y), R = (pow(x, 1 / 3.0) * sin(x)/(pow(exp(1.0),3 * x) + pow(exp(1.0), 3 * y)));
+	float S = tan(x / y);
+	float R = (pow(x, 1 / 3.0) * sin(x)/(pow(exp(1.0),3 * x) + pow(exp(1.0), 3 * y)));
 
 	float C = max(R, S);
 
